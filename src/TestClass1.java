bd9ec9a4571261f372b211749fee75981fa28dff
Steve Horton <Steve Horton@LAPTOP-OLHJ58RE>
2020-10-27 12:10:45 -0700

    Team member commit

--- src/TestClass1.java
+++ src/TestClass1.java
@@ -5,6 +5,11 @@ public class TestClass1 {
 		// TODO Auto-generated method stub
 		System.out.println("This is fist Git project");
 		System.out.println("for Java Projects");
+		System.out.println("new code addedd");
+		int a = 10;
+		if(a<20) {
+			System.out.println("a < 20");
+		}
 
 	}
 
