5a319bcb0c3ed81d7ff42d0a706779a3a1973fc1
Steve Horton <Steve Horton@LAPTOP-OLHJ58RE>
2020-10-27 11:48:27 -0700

    First Commit

---
+++ src/TestClass1.java
@@ -0,0 +1,11 @@
+
+public class TestClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is fist Git project");
+		System.out.println("for Java Projects");
+
+	}
+
+}
